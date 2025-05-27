451ee2567ba8fa3f5565c4c46d219e98e74af184
muyanja365 <muyanjarobert04@gmail.com>
2025-05-27 19:39:40 +0300

    location change

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "@/components/navbar/Navbar";
 import HeroSection from "@/components/hero/HeroSection";
+import HeroSection2 from "@/components/intro/HeroSection2";
 
 const HomePage: React.FC = () => {
   return (
    <>
    <Navbar />
     <HeroSection />
+    <HeroSection2 />
    </>
   );
 };
