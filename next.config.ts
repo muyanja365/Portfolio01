8350ab5a116f9e45000208c316d8978ed0e1ae4a
muyanja365 <muyanjarobert04@gmail.com>
2025-05-27 21:36:51 +0300

    new

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from "next";
-import withFlowbiteReact from "flowbite-react/plugin/nextjs";
+// import type { NextConfig } from "next";
+// import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // required to work with next/image on static sites
+  },
+  trailingSlash: true, // ensure URLs work properly on GitHub Pages
+  basePath: '/Portfolio01', // this matches your GitHub repo name
 };
 
-export default withFlowbiteReact(nextConfig);
+export default nextConfig;
